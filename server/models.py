7b10f43658665e1a57d43b4abb294ee432aef46a
Benjamin Okello <benjamin.okello@student.moringaschool.com>
2024-10-11 18:02:19 +0300

    passed validatoins

--- server/models.py
+++ server/models.py
@@ -4,21 +4,38 @@ db = SQLAlchemy()
 
 class Author(db.Model):
     __tablename__ = 'authors'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String, unique=True, nullable=False)
-    phone_number = db.Column(db.String)
+    phone_number = db.Column(db.String(10))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    # Add validators
+
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Author name must not be empty.')
+        existing_name = Author.query.filter_by(name=name).first()
+        if existing_name:
+            raise ValueError('Author already exist')
+        return name
+
+    @validates('phone_number')
+    def validate_number(self,key, phone_number):
+        if len(phone_number) != 10 or not phone_number.isdigit():
+            raise ValueError('Phone number must be 10 characters long.')
+        return phone_number
+
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
 class Post(db.Model):
     __tablename__ = 'posts'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     content = db.Column(db.String)
@@ -27,8 +44,32 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    # Add validators
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250 :
+            raise ValueError('Add more content.')
 
+        return content
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError('Provide a shorter summary.')
+        return summary
+
+    @validates('category')
+    def validat_categrory(self, key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError('Category must be either Fiction or Non-Fiction.')
+        return category
+
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait_keywords = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(keyword in title for keyword in clickbait_keywords):
+            raise ValueError('Title must contain at least one clickbait keyword: "Won\'t Believe", "Secret", "Top", or "Guess".')
+        return title
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
